d25f4bc782084ddfb35d5a663de7095974cdab7b
transportesnimblex-lang <transportesnimblex@gmail.com>
2026-08-14 13:14:55 -0600

    Update MainActivity.kt

--- app/src/main/java/com/nimblex/facebookprospector/MainActivity.kt
+++ app/src/main/java/com/nimblex/facebookprospector/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.nimblex.facebookprospector
 
 import android.app.Activity
-mport android.content.*
+import android.content.*
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
